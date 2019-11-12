3b5c76807a03410b43b2d1f480cd3a88a8536551
Leticia Berardo <leticiaberardo@gmail.com>
2019-11-12 14:15:25 -0300

    Tarjeta de credio con validacion para numero de 16 digitos con o sin espacios

--- tarjeta-de-credito/js/tdc.js
+++ tarjeta-de-credito/js/tdc.js
@@ -1,55 +1,13 @@
-$(function() {
-    $('form.require-validation').bind('submit', function(e) {
-      var $form         = $(e.target).closest('form'),
-          inputSelector = ['input[type=email]', 'input[type=password]',
-                           'input[type=text]', 'input[type=file]',
-                           'textarea'].join(', '),
-          $inputs       = $form.find('.required').find(inputSelector),
-          $errorMessage = $form.find('div.error'),
-          valid         = true;
-  
-      $errorMessage.addClass('hide');
-      $('.has-error').removeClass('has-error');
-      $inputs.each(function(i, el) {
-        var $input = $(el);
-        if ($input.val() === '') {
-          $input.parent().addClass('has-error');
-          $errorMessage.removeClass('hide');
-          e.preventDefault(); // cancel on first error
-        }
-      });
-    });
-  });
-  
-  $(function() {
-    var $form = $("#payment-form");
-  
-    $form.on('submit', function(e) {
-      if (!$form.data('cc-on-file')) {
-        e.preventDefault();
-        Stripe.setPublishableKey($form.data('stripe-publishable-key'));
-        Stripe.createToken({
-          number: $('.card-number').val(),
-          cvc: $('.card-cvc').val(),
-          exp_month: $('.card-expiry-month').val(),
-          exp_year: $('.card-expiry-year').val()
-        }, stripeResponseHandler);
-      }
-    });
-  
-    function stripeResponseHandler(status, response) {
-      if (response.error) {
-        $('.error')
-          .removeClass('hide')
-          .find('.alert')
-          .text(response.error.message);
-      } else {
-        // token contains id, last4, and card type
-        var token = response['id'];
-        // insert the token into the form so it gets submitted to the server
-        $form.find('input[type=text]').empty();
-        $form.append("<input type='hidden' name='reservation[stripe_token]' value='" + token + "'/>");
-        $form.get(0).submit();
-      }
-    }
-  })
+//Tarjeta event listener
+
+$('#carga-formulario').click(getCardValue);
+
+function getCardValue() {
+  let inputCard = document.getElementById('card-number').value;
+  let regex = /^[0-9]{15,16}|(([0-9]{4}\s){3}[0-9]{3,4})$/;
+  if (regex.test(inputCard)) {
+    console.log('es valida');
+  } else {
+    console.log('no es valida');
+  }
+}
